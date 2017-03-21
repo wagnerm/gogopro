e785014f8ef37cce066b7b25c845ec37e3acf019
Max Wagner <maxwag56@gmail.com>
2017-03-20 20:19:00 -0600

    Move status command to generic object
    - The StatusCommand type now runs api command and determines the
      correct value to return.
    - The Power type will create predetermined StatusCommands with
      translaters. Power functions will use the correct StatusCommand
      matching the function called.

--- power.go
+++ power.go
@@ -1,11 +1,10 @@
 package gogopro
 
-import (
-	"io/ioutil"
-)
+import ()
 
 type Power struct {
-	APIRequester *APIRequester
+	APIRequester   *APIRequester
+	StatusCommands map[string]StatusCommand
 }
 
 func (p *Power) Init() *Power {
@@ -15,18 +14,28 @@ func (p *Power) Init() *Power {
 func CreatePower(APIRequester *APIRequester) *Power {
 	power := &Power{}
 	power.APIRequester = APIRequester
+	statusCommands := CreateStatusCommands()
+	power.StatusCommands = statusCommands
 	return power
 }
 
+func CreateStatusCommands() map[string]StatusCommand {
+	sc := make(map[string]StatusCommand)
+	sc["power"] = StatusCommand{Endpoint: "/bacpac/se", ResultByte: -1,
+		Translaters: []StatusTranslater{
+			StatusTranslater{
+				Result:         0,
+				ExpectedReturn: "off"},
+			StatusTranslater{
+				Result:         1,
+				ExpectedReturn: "on"}}}
+	return sc
+}
+
 func (p *Power) GetPowerStatus() (string, error) {
-	resp, err := p.APIRequester.get("/bacpac/se")
+	result, err := p.StatusCommands["power"].RunStatusCommand(p.APIRequester)
 	if err != nil {
 		return "", err
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if body[len(body)-1] == 0 {
-		return "off", nil
-	}
-	return "on", nil
+	return result, nil
 }
